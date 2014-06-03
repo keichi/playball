eae66d9efb0193c1bfbc960fc8a480d6a3e9db88
Keichi Takahashi <keichi.t@me.com>
2014-06-03 16:39:21 +0900

    DOAにtoCSVメソッドを追加した

--- app/DOA.scala
+++ app/DOA.scala
@@ -1,10 +1,12 @@
 package play.boy.doa
 
+import scala.reflect.runtime.{universe => ru}
 import play.api.db.slick.Config.driver.simple._
 
-private[doa] object PlayboyDuck {  
+private[doa] object Duck {  
   type Model[A] = {
     val id: Option[Long]
+    def productIterator: Iterator[Any]
   }
   type Table = {
     def id: Column[Long]
@@ -12,7 +14,7 @@ private[doa] object PlayboyDuck {
 }
 
 // A: モデルクラス, B: Aをmappingするテーブル定義クラス
-abstract class DOA[A <: PlayboyDuck.Model[A], B <: Table[A] with PlayboyDuck.Table] {
+abstract class DOA[A <: Duck.Model[A] : ru.TypeTag, B <: Table[A] with Duck.Table] {
   protected val query: TableQuery[B]
 
   def list(implicit s: Session): List[A] = {
@@ -28,17 +30,39 @@ abstract class DOA[A <: PlayboyDuck.Model[A], B <: Table[A] with PlayboyDuck.Tab
   }
 
   def toCSV(implicit s: Session): String = {
-    ""
+    val builder = new StringBuilder()
+    
+    query.list.foreach({ row: A =>
+      row.productIterator.foreach(c => builder ++= c.toString += ',')
+      builder += '\n'
+    })
+
+    builder.toString
   }
 
   def findById(id: Long)(implicit s: Session): Option[A] = {
     query.where(_.id === id).firstOption
   }
 
-  // def update(id: Long, item: A)(implicit s: Session) {
-  //   val newItem: A = item.copy(Some(id))
-  //   query.where(_.id === id).update(newItem)
-  // }
+  def update(id: Long, item: A)(implicit s: Session, ct: scala.reflect.ClassTag[A]) = {
+    lazy val runtimeMirror = ru.typeTag[A].mirror
+    val instanceMirror = runtimeMirror.reflect(item)
+
+    lazy val method = ru.typeOf[A].member(ru.newTermName("copy")).asMethod
+    val methodMirror = instanceMirror.reflectMethod(method)
+
+    lazy val fields = ru.typeOf[A].members.filter(_.isTerm).map(_.asTerm).filter(_.isAccessor)
+    val fieldValues = fields.map(f =>
+      if (f.name.decoded == "id") {
+          Some(id)
+        } else {
+          instanceMirror.reflectField(f).get
+      }).toSeq.reverse
+
+    val newItem = methodMirror(fieldValues:_*).asInstanceOf[A]
+
+    query.where(_.id === id).update(newItem)
+  }
 
   def delete(id: Long)(implicit s: Session) {
     query.where(_.id === id).delete
