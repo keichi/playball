ea99c90c92bae32958686383afb568e6b02a2622
Keichi Takahashi <keichi.t@me.com>
2014-06-19 20:41:20 +0900

    RESTful APIの試作

--- app/Global.scala
+++ app/Global.scala
@@ -3,7 +3,11 @@ import play.api.mvc.{Handler, RequestHeader}
 import play.api.Play.current
 import play.api.db.slick._
 
+import scala.reflect.runtime.currentMirror
+import scala.reflect.runtime.universe._
+
 import models._
+import play.boy.dao._
 
 object Global extends GlobalSettings {
 
@@ -26,9 +30,32 @@ object Global extends GlobalSettings {
   }
 
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
-    lazy val regex = """/api/""".r
+    if (request.path.startsWith("/api/")) {
+      lazy val regex = """/api/([^/]+)(/(\d+))?/?""".r
+      val regex(model, _, id) = request.path
 
-    super.onRouteRequest(request)
+      val moduleSymbol = currentMirror.staticPackage("models").typeSignature.members
+        .filter(_.isModule)
+        .filter(_.typeSignature.baseClasses.contains(typeOf[DAO[_, _]].typeSymbol))
+        .find(moduleSymbol => {
+          val baseType = moduleSymbol.typeSignature.baseType(typeOf[DAO[_, _]].typeSymbol)
+          val TypeRef(_, _, List(modelType, tableType)) = baseType
+
+          modelType.typeSymbol.name.decoded.toLowerCase == model
+        }).get.asModule
+
+      val dao = currentMirror.reflectModule(moduleSymbol).instance.asInstanceOf[DAO[_, _]]
+
+      request.method match {
+        case "GET" if id == null => Some(controllers.REST.index(dao))
+        case "GET" => Some(controllers.REST.get(dao, id.toLong))
+        case "POST" => Some(controllers.REST.create(dao))
+        case "DELETE" => Some(controllers.REST.delete(dao, id.toLong))
+        case _ => super.onRouteRequest(request)
+      }
+    } else {
+      super.onRouteRequest(request)
+    }    
   }
 
 }
--- app/controllers/REST.scala
+++ app/controllers/REST.scala
@@ -1,24 +1,28 @@
+package controllers
+
 import play.api._
 import play.api.mvc._
 import play.api.db.slick._
+import play.api.libs.json.Json
 
 import models._
 import views._
+import play.boy.dao._
 
-object Application extends Controller {
-  def index = DBAction { implicit s =>
+object REST extends Controller {
+  def index(dao: DAO[_, _]) = DBAction { implicit s =>
+    Ok(Json.toJson(dao.list.asInstanceOf[List[BeerBrand]]))
+  }
+  def create(dao: DAO[_, _]) = DBAction { implicit s =>
     Ok("")
   }
-  def create = DBAction { implicit s =>
+  def get(dao: DAO[_, _], id: Long) = DBAction { implicit s =>
+    Ok(Json.toJson(dao.findById(id).asInstanceOf[Option[BeerBrand]]))
+  }
+  def update(dao: DAO[_, _], id: Long) = DBAction { implicit s =>
     Ok("")
   }
-  def get(id: Long) = DBAction { implicit s =>
-    Ok("")
-  }
-  def update(id: Long) = DBAction { implicit s =>
-    Ok("")
-  }
-  def delete(id: Long) = DBAction { implicit s =>
+  def delete(dao: DAO[_, _], id: Long) = DBAction { implicit s =>
     Ok("")
   }
 }
