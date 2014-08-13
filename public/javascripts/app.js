4dc2a45b9ee79c628367ce37456f2047bdc4e2e1
Keichi Takahashi <keichi.t@me.com>
2014-08-13 16:43:07 +0900

    フラッシュを使ってエラーメッセージなどを表示できるようにした

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,12 @@
 var beerApp = angular.module('BeerApp', []);
 
+beerApp.controller('FlashMessageCtrl', function($scope, $http) {
+	$scope.showSuccess = true;
+	$scope.showInfo = true;
+	$scope.showWarning = true;
+	$scope.showError = true;
+});
+
 beerApp.controller('BeerListCtrl', function ($scope, $http) {
 	$http.get('/api/beerbrand/', {
 		params: {
