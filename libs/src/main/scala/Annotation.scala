e447727f89ae20813da72c0eaab1635866bd9546
Keichi Takahashi <keichi.t@me.com>
2014-08-20 22:41:09 +0900

    モデルの認証処理のためのアノテーションクラスを追加

--- libs/src/main/scala/Annotation.scala
+++ libs/src/main/scala/Annotation.scala
@@ -1,7 +1,18 @@
 package play.boy.annotation
 
 import scala.annotation.StaticAnnotation
+import play.boy.auth.RoleLike
+
+object Permission {
+  case object Read extends Permission
+  case object Write extends Permission
+}
+
+sealed abstract trait Permission {
+}
 
 // modelクラスのメンバに使えるアノテーション
 case class label(message: String) extends StaticAnnotation
 case class text(rows: Int) extends StaticAnnotation
+case class owner(perms: Permission*) extends StaticAnnotation
+case class authorize(role: RoleLike#Value, perms: Permission*) extends StaticAnnotation
